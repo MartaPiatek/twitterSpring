de9d4a19a3a93009a1782f3071719d57131e6cf6
MartaPiatek <martusia.piatek@gmail.com>
2017-09-30 18:59:29 +0200

    Dodanie strony wyszukiwania tweetów

--- src/main/java/pl/martapiatek/controller/TweetController.java
+++ src/main/java/pl/martapiatek/controller/TweetController.java
@@ -2,7 +2,9 @@ package pl.martapiatek.controller;
 
 
 import java.util.List;
-import java.util.stream.Collectors;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.twitter.api.SearchResults;
 import org.springframework.social.twitter.api.Tweet;
@@ -10,7 +12,9 @@ import org.springframework.social.twitter.api.Twitter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class TweetController {
@@ -18,18 +22,27 @@ public class TweetController {
 	@Autowired
 	private Twitter twitter;
 	
-
 	@RequestMapping("/")
+	public String home() {
+		return "searchPage";
+	}
+
+	@RequestMapping("/result")
 	public String hello(@RequestParam(defaultValue = "TajnikiSpringMVC4") String search, 
 			Model model) {
 		SearchResults searchResults = twitter.searchOperations().search(search);
-		
 		List<Tweet> tweets = searchResults.getTweets();
-		
 		model.addAttribute("tweets", tweets);
 		model.addAttribute("search", search);
 		
 		return "resultPage";
+	}
+	
+	@RequestMapping(value = "/postSearch", method = RequestMethod.POST)
+	public String postSearch(HttpServletRequest request, RedirectAttributes redirectAttributes) {
+		String search = request.getParameter("search");
+		redirectAttributes.addAttribute("search", search);
+		return "redirect:result";
 	}
 	
 }
